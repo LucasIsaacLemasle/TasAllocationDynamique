e93e825bd4f3dd008d0b610665c5040f94bb9c34
LucasIsaacLemasle <li2003@hw.ac.uk>
2023-04-05 15:21:21 +0200

    last commit

--- main.c
+++ main.c
@@ -3,17 +3,15 @@
 #include "affiche_tas.h"
 #include <string.h>
 
-//#include <sys/stat.h>
-//
-//#include <CUnit/CUnit.h>
-//#include <CUnit/Basic.h>
 
-
+// Macros
 #define buffer_dans_tas(ptr)         ptr<(tas+TAILTAS-1)
 #define buffer_libre(ptr)            *(ptr+1)==-1
 #define taille_buffer_ok(taille,ptr) taille<*ptr
 
 
+
+// Strategie first fit
 char* first_fit(unsigned int taille)
 {
     char* ptr = tas;
@@ -37,6 +35,7 @@ char* first_fit(unsigned int taille)
     return ptr;
 }
 
+// Strategie best fit
 char* best_fit(unsigned int taille)
 {
     char* ptr = tas;
@@ -60,6 +59,7 @@ char* best_fit(unsigned int taille)
     return petit_ptr;
 }
 
+// Strategie worst fit
 char* worst_fit(unsigned int taille)
 {
     char* ptr = tas;
@@ -83,36 +83,10 @@ char* worst_fit(unsigned int taille)
     return grand_ptr;
 }
 
-char* tas_malloc(unsigned int taille)
-{
-//    char* pos_allo = first_fit(taille);
-    char* pos_allo = best_fit(taille);
-//    char* pos_allo = worst_fit(taille);
-    if(pos_allo == (tas + TAILTAS -1))// vérifie qu'il y a toujours de l'espace dans le tas
-    {
-        printf("Espace insuffisant\n");
-        return pos_allo;
-    }
-
-    if(TAILTAS - *pos_allo == libre) // vérification du dernier buffer libre
-    {
-        printf("coucou\n");
-        libre = libre + taille + 1; // mise à jour de la variable libre
-        *(pos_allo + taille + 1) = TAILTAS - libre; // notation de l'espace libre restant à la suite
-        *(pos_allo + taille + 2) = -1; // informe que cet espace est libre
-
-    }
-    // on ajoute une case si elle se retrouve seule entre deux buffers ou à la fin
-    if(*pos_allo - taille == 1)
-    {
-       taille++;
-    }
-    *pos_allo = taille; // attribution de la taille allouée
-
-    return pos_allo + 1;
-}
 
 
+/* Fonction malloc qui prend en argument la taille à allouer ainsi
+    qu'un pointeur de fonction de fonction, représentant la strategie choisie */
 char* tas_malloc_fct(unsigned int taille, char* (pt_fct)(unsigned int))
 {
 
@@ -123,22 +97,22 @@ char* tas_malloc_fct(unsigned int taille, char* (pt_fct)(unsigned int))
         return pos_allo;
     }
 
-    if(TAILTAS - *pos_allo == libre) // vérification du dernier buffer libre
+    if(TAILTAS - *pos_allo == libre) // verification du dernier buffer libre
     {
-        printf("coucou\n");
         libre = libre + taille + 1; // mise à jour de la variable libre
         *(pos_allo + taille + 1) = TAILTAS - libre; // notation de l'espace libre restant à la suite
         *(pos_allo + taille + 2) = -1; // informe que cet espace est libre
 
     }
-    // on ajoute une case si elle se retrouve seule entre deux buffers ou à la fin
+    // on ajoute une case si elle se retrouve seule entre deux buffers ou a la fin
+    // car il faut au moins deux cases pour etre considere libre
     if(*pos_allo - taille == 1)
     {
        taille++;
     }
-    *pos_allo = taille; // attribution de la taille allouée
+    *pos_allo = taille; // attribution de la taille allouee
 
-    return pos_allo + 1;
+    return pos_allo + 1; // retour de l'adresse de la position juste apres
 }
 
 
@@ -147,15 +121,12 @@ void tas_free(char* ptr)
     *ptr = -1; // le pointeur pointe sur le début du bloc, juste après sa taille
 }
 
-int get_block_size(char* ptr)
-{
-    return *(ptr-1);
-}
 
 int main()
 {
+    // initialisation du tas
     tas_init();
-    int pred = tas;
+//    int pred = tas;
 
 /* Pointeurs des fonctions best-fits, worst-fits, first-fits */
     char* (*pt_fct_best)(unsigned int) = &best_fit;
@@ -163,68 +134,28 @@ int main()
     char* (*pt_fct_worst)(unsigned int) = &worst_fit;
 
 
-/* Premier Essai */
-//    afficher_tas();
-//
-//    char* ptr1 = (char*)tas_malloc(10);
-//    strcpy(ptr1,"hello");
-//    afficher_tas();
-//
-//    char* ptr2 = (char*) tas_malloc(7);
-//    strcpy(ptr2,"world");
-//    afficher_tas();
-
-/* Affichage de l'énoncé */
-/*
-    char *p1, *p2, *p3, *p4;
-    p1 = (char *) tas_malloc(10);
-    strcpy( p1, "tp 1" );
-    afficher_tas();
-
-    p2 = (char *) tas_malloc(9);
-    strcpy( p2, "tp 2" );
-    afficher_tas();
-
-    p3 = (char *) tas_malloc(5);
-    strcpy( p3, "tp 3" );
-    afficher_tas();
-
-    tas_free(p2);
-    afficher_tas();
-
-    p4 = (char *) tas_malloc(8);
-    strcpy( p4, "systeme" );
-    afficher_tas();
-*/
 
 /* Affichage de l'énoncé avec des pointeurs de fonctions */
 
     char *p1, *p2, *p3, *p4;
+
     p1 = (char *) tas_malloc_fct(10,pt_fct_best);
     strcpy( p1, "tp 1" );
-    afficher_tas();
 
     p2 = (char *) tas_malloc_fct(9,pt_fct_best);
     strcpy( p2, "tp 2" );
-    afficher_tas();
 
     p3 = (char *) tas_malloc_fct(5,pt_fct_best);
     strcpy( p3, "tp 3" );
-    afficher_tas();
 
     tas_free(p2);
-    afficher_tas();
 
     p4 = (char *) tas_malloc_fct(8,pt_fct_best);
     strcpy( p4, "systeme" );
+
+
     afficher_tas();
 
-
-
-///* TEST */
-//    CU_ASSERT(get_block_size(p1) == 10);
-//    CU_ASSERT(get_block_size(p4) == 9);
-//    CU_ASSERT(get_block_size(p3) == 5);
 
     return 0;
 }
